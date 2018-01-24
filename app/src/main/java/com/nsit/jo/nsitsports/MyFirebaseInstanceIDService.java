c1b2b1aaaaadcf7c9d637d0d1be1815b848fc38e
jO <karanjot21@gmail.com>
2018-01-24 22:00:24 +0530

    Init. Notify Thingy

--- app/src/main/java/com/nsit/jo/nsitsports/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/nsit/jo/nsitsports/MyFirebaseInstanceIDService.java
@@ -1,5 +1,7 @@
 package com.nsit.jo.nsitsports;
 
+import android.util.Log;
+
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -8,11 +10,12 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
  */
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    final String TAG = "notify";
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
--- app/src/main/java/com/nsit/jo/nsitsports/MyFirebaseMessagingService.java
+++ app/src/main/java/com/nsit/jo/nsitsports/MyFirebaseMessagingService.java
@@ -8,15 +8,14 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import static android.content.ContentValues.TAG;
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    final String TAG = "notify";
+
     public MyFirebaseMessagingService() {
     }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
